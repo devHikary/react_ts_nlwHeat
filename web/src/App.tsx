1fe1557f9e5ca6af7ad89302a18ca4256913e99b
devHikary <wrkkarina@gmail.com>
2021-10-24 08:55:48 -0300

    Component SendMessage

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,17 @@
+import { useContext } from 'react'
 import style from './App.module.scss'
 import { LoginBox } from './components/LoginBox'
 import { MessageList } from './components/MessageList'
+import { SendMessageForm } from './components/SendMessageForm'
+import { AuthContext } from './context/auth'
 
 export function App() {
+  const {user}=useContext(AuthContext);
 
   return (
     <main className={style.contentWrapper}>
       <MessageList />
-      <LoginBox />
+      { !!user? <SendMessageForm/> : <LoginBox />}
     </main>
   )
 }
